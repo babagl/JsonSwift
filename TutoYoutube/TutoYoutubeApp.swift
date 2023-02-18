04a13556e472a05f20fcb8fe33e238a732db4d45
Abdoulaye Aliou SALL <gallebaba71@gmail.com>
2023-02-18 14:52:11 +0000

    Initial Commit

---
+++ TutoYoutube/TutoYoutubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutoYoutubeApp.swift
+//  TutoYoutube
+//
+//  Created by Abdoulaye Aliou SALL on 18/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct TutoYoutubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
